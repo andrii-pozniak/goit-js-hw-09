d0c032e939693819eb881b831ff5309b2c15dec5
Andrii Pozniak <andriipozniak@gmail.com>
2022-09-10 18:01:34 +0300

    end task2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,47 +3,39 @@ import "flatpickr/dist/flatpickr.min.css";
 
 const input = document.querySelector(`#datetime-picker`);
 const startBtn = document.querySelector(`button[data-start]`);
-const timerStyle = document.querySelector(".timer");
-
 const timerNumber = document.querySelector(`span[data-days]`);
 const hoursNumber = document.querySelector(`span[data-hours]`);
 const minutesNumber = document.querySelector(`span[data-minutes]`)
 const secondsNumber = document.querySelector(`span[data-seconds]`)
 
 
-console.log(timerNumber)
-// addLeadingZero();
-
 const fp = flatpickr("#datetime-picker",options = {
     enableTime: true,
     time_24hr: true,
     defaultDate: new Date(),
     minuteIncrement: 1,
-   count: null,
+   
     onClose(selectedDates) {
      
-      const currentTime = selectedDates[0];
-    const starTime = Date.now();
-  
-    this.count = currentTime - starTime;
-   
+        const currentTime = selectedDates[0];
+        const starTime = Date.now();
+     
     if (currentTime <= starTime) {
         window.alert("Please choose a date in the future");
+        
+        return;
+        
     }
-    // console.log(this.count)
+    startBtn.addEventListener(`click`, () => {
+        timer.start();
+    
+    });
+       
     },
    
-  }); 
-//   console.log(this.count)
-  
-input.addEventListener(`click`, onClickInput);
-
-function onClickInput(evn) {  
-    evn.currentTarget.value = fp;   
-}
-
-startBtn.addEventListener(`click`, onClickStartBtn => {
-    timer.start();
+  });   
+ 
+input.addEventListener(`click`, () => {
 
 });
 
@@ -65,15 +57,8 @@ function convertMs(ms) {
   
     return { days, hours, minutes, seconds };
   }
-  
-//   console.log("time", (convertMs(2000))); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-//   console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-//   console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
-convertMs();
 
 function padStart(value) {
-    // console.log(String(value).padStart(2, '0'))
-
     return String(value).padStart(2, '0')
 };
 
@@ -84,13 +69,14 @@ class Timer {
     }
    
     start () {
-        const onStart = Date.now();
+        
         if (this.isActive) {
             return;
         }
         this.isActive = true;
         setInterval(() => {
-            const currentTime = Date.now();
+            const onStart = Date.now();
+            const currentTime = fp.selectedDates[0];
             const deltaTime = currentTime - onStart;
             const { days, hours, minutes, seconds } = convertMs(deltaTime);
             timerNumber.textContent = `${padStart(days)}`
@@ -99,9 +85,7 @@ class Timer {
             minutesNumber.textContent = `${padStart(minutes)}`
             hoursNumber.textContent = `${padStart(hours)}`
             secondsNumber.textContent = `${padStart(seconds)}`
-            console.log(timerNumber.dataset.days)
-            console.log(`${days}:${hours}:${minutes}:${seconds}`)
-
+           
         }, 1000);
     }
 }
