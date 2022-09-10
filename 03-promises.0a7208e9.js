fabf5e33164d3f22b109c5c11084d76ca0628d63
andrii-pozniak <103370189+andrii-pozniak@users.noreply.github.com>
2022-09-10 19:56:01 +0000

    Deploying to gh-pages from @ andrii-pozniak/goit-js-hw-09@848b63bf2413fb338d6b592a7889eb37b635e66b 🚀

---
+++ 03-promises.0a7208e9.js
@@ -0,0 +1,2 @@
+document.querySelector('input[name="delay"]'),document.querySelector('input[name="step"]'),document.querySelector('input[name="amount"]');
+//# sourceMappingURL=03-promises.0a7208e9.js.map
---
+++ 03-promises.943bf879.js
@@ -0,0 +1,2 @@
+document.querySelector('input[name="delay"]'),document.querySelector('input[name="step"]'),document.querySelector('input[name="amount"]');
+//# sourceMappingURL=03-promises.943bf879.js.map
