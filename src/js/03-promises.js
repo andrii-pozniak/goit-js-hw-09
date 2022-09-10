848b63bf2413fb338d6b592a7889eb37b635e66b
Andrii Pozniak <andriipozniak@gmail.com>
2022-09-10 22:55:28 +0300

    start task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,4 +1,19 @@
+const refs = {
+  inputDelay: document.querySelector(`input[name="delay"]`),
+  inputStep: document.querySelector(`input[name="step"]`),
+  inputAmount: document.querySelector(`input[name="amount"]`),
+
+}
+
 function createPromise(position, delay) {
+  return new Promise(resolve => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      resolve({position, delay})
+      
+    }, delay);
+  })
+
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
     // Fulfill
