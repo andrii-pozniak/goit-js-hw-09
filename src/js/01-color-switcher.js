d11ef96ce33bd484a6ecb7c14240b02048aeff7b
Andrii Pozniak <andriipozniak@gmail.com>
2022-09-16 00:12:26 +0300

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,14 +1,24 @@
+
 const startBtn  = document.querySelector(`button[data-start]`);
 const stopBtn  = document.querySelector(`button[data-stop]`);
-const bodyStyle = document.querySelector(`body`)
+const bodyStyle = document.querySelector(`body`);
 
-console.log(bodyStyle)
+stopBtn.disabled = false;
+
+
 let timerid = null;
 
-startBtn.addEventListener (`click`, () => {timerid = setInterval(() => { 
-    bodyStyle.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`
+startBtn.addEventListener (`click`, () => {
+  
 
-}, 1000)} );
+  timerid = setInterval(() => { 
+    bodyStyle.style.backgroundColor = getRandomHexColor();
+
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+}, 1000);
+
+} );
 
 function getRandomHexColor() {
 console.log(`#${Math.floor(Math.random() * 16777215).toString(16)}`)
@@ -16,6 +26,8 @@ console.log(`#${Math.floor(Math.random() * 16777215).toString(16)}`)
   };
  
   stopBtn.addEventListener(`click`, () => {
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
     clearInterval(timerid)
   })
 
