93d20149dd0f328fa8e4e198c811d7f4b8968797
andrii-pozniak <andriipozniak@gmail.com>
2022-09-18 17:43:58 +0300

    change task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -7,17 +7,24 @@ stopBtn.disabled = false;
 
 
 let timerid = null;
+let throttle = require('lodash.throttle');
+function stopClick() {
+  
+    const stopClickBtn = throttle(startBtn, 500)
+  }
 
-startBtn.addEventListener (`click`, () => {  
 
+startBtn.addEventListener (`click`, () => {    
+ 
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
   timerid = setInterval(() => { 
     bodyStyle.style.backgroundColor = getRandomHexColor();
 
-    startBtn.disabled = true;
-    stopBtn.disabled = false;
+    
 }, 1000);
 
-} );
+});
 
 function getRandomHexColor() {
 console.log(`#${Math.floor(Math.random() * 16777215).toString(16)}`)
