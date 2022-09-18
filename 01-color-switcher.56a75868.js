5aa75db20de482b669fb5fd60c52d3e8ca7f9804
andrii-pozniak <103370189+andrii-pozniak@users.noreply.github.com>
2022-09-18 14:44:30 +0000

    Deploying to gh-pages from @ andrii-pozniak/goit-js-hw-09@93d20149dd0f328fa8e4e198c811d7f4b8968797 🚀

--- 01-color-switcher.56a75868.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.querySelector("body");e.disabled=!1;let d=null;t.addEventListener("click",(()=>{d=setInterval((()=>{o.style.backgroundColor=(console.log(`#${Math.floor(16777215*Math.random()).toString(16)}`),`#${Math.floor(16777215*Math.random()).toString(16)}`),t.disabled=!0,e.disabled=!1}),1e3)})),e.addEventListener("click",(()=>{t.disabled=!1,e.disabled=!0,clearInterval(d)}));
-//# sourceMappingURL=01-color-switcher.56a75868.js.map
